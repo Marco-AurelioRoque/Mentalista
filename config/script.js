2492a34a4f8a20495ac8880f0e905a8c322d8b9d
Marco-AurelioRoque <marcoaureliocraft@gmail.com>
2021-05-12 11:56:31 -0300

    update

--- config/script.js
+++ config/script.js
@@ -1,1 +1,12 @@
+var palpitePessoa = document.querySelector(".chute").value
 
+var tentativas = 10
+
+function verificarChute() {
+    if(palpitePessoa == parseInt(Math.random(1) * 10)) {
+        console.log("ACERTOU!")
+        
+    } else {
+        console.log("ERROU!")
+    }
+}
