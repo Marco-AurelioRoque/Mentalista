1e3a99fe4e678174849beb44993e4abc61352c95
Marco-AurelioRoque <marcoaureliocraft@gmail.com>
2021-05-13 18:16:56 -0300

    Update

--- config/script.js
+++ config/script.js
@@ -1,12 +1,13 @@
 var palpitePessoa = document.querySelector(".chute").value
-
+var numeroSecreto = parseInt(Math.random(1) * 10)
 var tentativas = 10
+var resultado = document.querySelector(".resultado-palpite")
 
 function verificarChute() {
-    if(palpitePessoa == parseInt(Math.random(1) * 10)) {
-        console.log("ACERTOU!")
+    if(palpitePessoa == numeroSecreto) {
+        resultado.innerHTML = "Você acertou";
         
     } else {
-        console.log("ERROU!")
+        resultado.innerHTML = "você errou!";
     }
 }
